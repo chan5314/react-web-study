8a644824335d88c5a5c3bbb2e6e36ecd4d946b40
이찬호 <ichanho@ichanhoui-MacBookPro.local>
2022-11-17 15:14:06 +0900

    비밀

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true})); //application/x-www-form-urlencoded 분석
+app.use(bodyParser.json()); //apllication/json 분석
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://chanho:ep2011ep!@boilerplate.epdavh2.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected!!'))
   .catch(err=> console.log('오류:',err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!, 변경!!')
 })
+
+app.post('/register',(req,res)=>{
+
+  //회원가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=> {
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
